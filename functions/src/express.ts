2b346669a8093360ab10e690b7741acd8dbeeace
Alex Iraola <alex.iraola@gmail.com>
2024-10-25 15:40:10 +0200

    Firebase admin reads Firestore data on dev and prod mode

--- functions/src/express.ts
+++ functions/src/express.ts
@@ -1,6 +1,7 @@
 import compression from "compression";
 import express from "express";
 import morgan from "morgan";
+import { firestore } from "./firebase.js";
 
 const app = express();
 
@@ -11,8 +12,9 @@ app.use(express.static("build/app/client", { maxAge: "1h" }));
 // http://expressjs.com/en/advanced/best-practice-security.html#at-a-minimum-disable-x-powered-by-header
 app.disable("x-powered-by");
 
-app.get("/ping", (_req, res) => {
-  res.send('Apa hi!');
+app.get("/ping", async (_req, res) => {
+  const data = await firestore.collection('prueba').get();
+  res.send(data.docs.map(d => d.data()));
 });
 
 export default app;
